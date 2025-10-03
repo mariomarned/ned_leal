a6b10842c538aad33d78c74390222cd867c85462
Mario Martinez <mmartinez@ned.mobi>
2025-10-02 20:57:58 -0500

    Pantalla del registro 1 completa, falta conectar a la pantalla siguiente

--- ned_leal_mobile/screens/Auth/RegisterScreen1.js
+++ ned_leal_mobile/screens/Auth/RegisterScreen1.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import CustomButton from '../../components/CustomButton';
 import { GlobalStyles, Colors, FontSizes, Spacing } from '../../constants/Styles'; // Corregido: se añadió Spacing
 
 const RegisterScreen1 = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={GlobalStyles.h1}>Registro</Text>
-      <Text style={GlobalStyles.bodyText}>¡Bienvenido! Comencemos tu registro.</Text>
-      {/* Placeholder for future content */}
+    <Image
+        source={require('../../assets/images/Auth/Logo_NED.png')}
+        style={styles.logo}
+    />
+      <Text style={GlobalStyles.title}>DISFRUTA EL PODER DE LEALTAD</Text>
       <CustomButton
-        title="Continuar"
+        title="INICIAR SESIÓN"
         onPress={() => navigation.navigate('Register3')} // Corregido: ahora navega a Register3
         style={styles.button}
+        textStyle={styles.buttonText}
+      />
+      <CustomButton
+        title="REGISTRARME"
+        onPress={() => navigation.navigate('Register2')}
+        style={[styles.button, { marginTop: Spacing.medium }]} // Añadir un margen superior para separarlo
+        textStyle={styles.buttonText}
       />
     </View>
   );
@@ -21,15 +30,33 @@ const RegisterScreen1 = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: Colors.white,
     padding: Spacing.medium, // Corregido
+    paddingTop: Spacing.large * 2,
+  },
+  logo: {
+    width: 180,
+    height: 180,
+    resizeMode: 'contain',
   },
   button: {
-    marginTop: Spacing.large, // Corregido
-    width: '80%',
+    marginTop: Spacing.large * 4.5,
+    backgroundColor: '#C4227D', // Pink color
+        paddingVertical: 15,
+        paddingHorizontal: 40,
+        borderRadius: 30,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
-
 export default RegisterScreen1;
